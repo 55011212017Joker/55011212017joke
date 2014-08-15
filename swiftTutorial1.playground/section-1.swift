f2dca37bfad83a8f48d4485d53e6c864e45d5a08
55011212017Joker <iMac@MSU>
2014-08-15 16:29:03 +0700

    ประเภทโค๊ต
    งานอาจารย์สั่งทำ

--- swiftTutorial1.playground/section-1.swift
+++ swiftTutorial1.playground/section-1.swift
@@ -4,4 +4,28 @@ let final = 77
 var sum = grade+final
     sum = sum+1
   print(sum)
-NSString * a = @"JAVA"
+//NSString * a = @"JAVA"
+
+let tutorialTeam = 56
+let editorialTeam = 23
+var totalTeam = tutorialTeam + editorialTeam
+
+totalTeam += 1
+
+let tutorialTeamInferred = 56
+let tutorialTeamExplicit: Int = 56
+
+let priceInferred = 19.99
+let priceExplicit:Double = 19.99
+
+let onSaleInferred = true
+let onSaleExplicit:Bool = false
+
+let nameInferred = "Whoopie Cushion"
+let nameExplicit:String = "Whoopie Cushion"
+
+if (onSaleInferred) {
+    println("\(nameInferred) on sale for \(priceInferred)!")
+} else {
+    println("\(nameInferred) at regular price: \(priceInferred)!")
+}
