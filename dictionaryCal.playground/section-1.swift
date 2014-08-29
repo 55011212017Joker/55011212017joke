b1a73889954b42208eb1aea9bf841b7a1f8e1e99
55011212017Joker <iMac@MSU>
2014-08-29 14:26:17 +0700

    new

--- dictionaryCal.playground/section-1.swift
+++ dictionaryCal.playground/section-1.swift
@@ -34,3 +34,16 @@ class TipCalculator{
 
 //
 let tipCalc = TipCalculator(total: 33.25, taxPct: 0.06)
+
+
+let array:Array<Int> = [1,2,3,4]
+let dictionary:Dictionary<String,Int> = ["dog":1,"elepphant":2]
+
+var airports:[String:String] = ["TY0":"Tokyo","DUB":"Dublin"]
+
+if (airports.isEmpty) {
+    println("The airports dictinary is empty")
+}else {
+     println("The airports dictinary is not empty ไม่มี")
+}
+
