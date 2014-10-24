7e0458f8ed7ac9aa0463daa0ec018237da0c6b77
55011212017joke <iMax@MSU>
2014-10-24 17:28:02 +0700

    seguerandomColor

--- segue/segue/ColorTwoViewController.swift
+++ segue/segue/ColorTwoViewController.swift
@@ -8,46 +8,55 @@
 
 import UIKit
 protocol ColorTwoViewControllerDelegate{
-    func myVCDidFinish(controller:ColorTwoViewController,text:String)
+    func myVCDidFinish(controller:ColorTwoViewController,text:String,procol:UIColor,f:UIFont,name:String,id:String)
 }
 
 class ColorTwoViewController: UIViewController {
     
+    @IBOutlet var red: UIButton!
+    
+    @IBOutlet var IDnameText: UITextField!
+    @IBOutlet var NameText: UITextField!
+    @IBOutlet var blue: UIButton!
+    @IBOutlet var green: UIButton!
     var delegate:ColorTwoViewControllerDelegate? = nil
     var ColorString = ""
-    var CR = [UIColor.redColor(),UIColor.greenColor(),UIColor.blueColor()]
-    
-    
-    @IBOutlet var ColorLable: UILabel!
-    
-    
+   @IBOutlet var ColorLable: UILabel!
+    var col: UIColor!
+    var fon: UIFont!
+   
     @IBAction func SaveColor(sender: UIBarButtonItem) {
         if(delegate != nil){
-            delegate!.myVCDidFinish(self, text: ColorLable!.text!)
+            delegate!.myVCDidFinish(self,text: ColorLable!.text!,procol:col,f:fon,name: NameText!.text!,id: IDnameText!.text!)
         }
         
     }
     
     @IBAction func ColorSelectionButton(sender: UIButton) {
         ColorLable.text = sender.titleLabel!.text!
-        ColorLable.textColor = getRandomColor()
+        col = getRandomColor();
+        ColorLable.textColor = col
         //ColorLable.backgroundColor = getRandomColor()
-        ColorLable.font = UIFont.italicSystemFontOfSize(20)
+        fon = UIFont.italicSystemFontOfSize(20)
+        ColorLable.font = fon
         ColorLable.layer.borderWidth = 5
-        ColorLable.layer.borderColor = getRandomColor().CGColor
+        ColorLable.layer.borderColor = col.CGColor
         
         
-            
+        
+        
     }
     func getRandomColor() -> UIColor{
         
-        var randomRed:CGFloat = CGFloat(drand48())
+        var randomRed = CGFloat(arc4random() % 255)
         
-        var randomGreen:CGFloat = CGFloat(drand48())
+        var randomGreen = CGFloat(arc4random() % 255)
         
-        var randomBlue:CGFloat = CGFloat(drand48())
+        var randomBlue = CGFloat(arc4random() % 255)
         
-        return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+        return UIColor(red: randomRed / 255.0, green: randomGreen / 255.0, blue: randomBlue / 255.0, alpha: 0.8)
+        
+        
         
     }
     
--- segue/segue/ViewController.swift
+++ segue/segue/ViewController.swift
@@ -8,23 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController,ColorTwoViewControllerDelegate {
+class ViewController: UIViewController,ColorTwoViewControllerDelegate{
 
     @IBOutlet var ColorLable: UILabel!
   
+    @IBOutlet var nameOne: UILabel!
+    
+    @IBOutlet var IDOne: UILabel!
+   
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       nameOne.text = ""
+       IDOne.text = ""
+        
+        
     }
-    func myVCDidFinish(controler:ColorTwoViewController,text:String){
+    func myVCDidFinish(controller:ColorTwoViewController,text:String,procol:UIColor,f:UIFont,name:String,id:String){
         ColorLable.text = "Co: " + text
-        controler.navigationController?.popViewControllerAnimated(true)
+        ColorLable.textColor = procol
+        ColorLable.font = f
+        ColorLable.layer.borderWidth = 5
+        ColorLable.layer.borderColor = procol.CGColor
+        nameOne.text = "NAME: " + name
+        nameOne.textColor = procol
+        nameOne.layer.borderWidth = 5
+        nameOne.layer.borderColor = UIColor.blackColor().CGColor
+        IDOne.text = "ID: " + id
+        IDOne.textColor = procol
+        IDOne.layer.borderWidth = 5
+        IDOne.layer.borderColor = UIColor.blackColor().CGColor
+        
+        
+        controller.navigationController?.popViewControllerAnimated(true)
         
         
     
     }
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "mySegue"{
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if (segue.identifier == "mysegue"){
             let vc = segue.destinationViewController as ColorTwoViewController
             vc.ColorString = ColorLable.text!
             vc.delegate = self
