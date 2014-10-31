6a84b9ff315870b2cf160f035f1ccac026981369
55011212017joke <iMax@MSU>
2014-10-31 16:41:50 +0700

    31/10/57 Coredata

---
+++ ProjectCoreSegue/ProjectCoreSegue/ViewControllerTWO.swift
@@ -0,0 +1,20 @@
+//
+//  ViewControllerTWO.swift
+//  ProjectCoreSegue
+//
+//  Created by Student on 10/31/14.
+//  Copyright (c) 2014 jay. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerTWO: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+   
+}
